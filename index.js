f063b4e981bd1f6ad2de74856812860925807997
TansanSui-H2CO3 <tansanwater.h2co3@gmail.com>
2021-03-03 02:09:09 +0900

    add: event router

--- index.js
+++ index.js
@@ -7,9 +7,37 @@ let http = require('http').Server(app);
 const io = require('socket.io')(http);
 const PORT = process.env.PORT || 8080; // Default port number
 
+// DB connection
+const pool = mysql.createPool({
+    host: 'host_name',
+    user: 'user_name',
+    password: 'password',
+    database: 'database',
+});
+pool.query = util.promisify(pool.query);
+
 // Routing
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/top.html');
+});
+
+io.on('connection', (socket) => {
+    console.log('User connected');
+    socket.on('book-list', (data) => {
+        
+    });
+    socket.on('read-book-list', (data) => {
+
+    });
+    socket.on('register-read-book', (data) => {
+
+    });
+    socket.on('register-review', (data) => {
+
+    });
+    socket.on('disconnect', () => {
+        console.log('User disconnected');
+    });
 });
 
 // Listening
